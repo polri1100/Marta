a3d9cb479072d90c500b9e80812401ee62788266
pol.rigau.ext@boehringer-ingelheim.com <pol.rigau.ext@boehringer-ingelheim.com>
2024-05-31 16:12:22 +0200

    secrets.toml

--- functions.py
+++ functions.py
@@ -18,7 +18,7 @@ credentials  = {"type": "service_account",
 
 def obtainSheet(sheetName):
 
-    client = pygsheets.authorize(service_account_file='credentials.json')
+    client = pygsheets.authorize(service_account_file='streamlit/secrets.toml')
 
     excel = client.open_by_url('https://docs.google.com/spreadsheets/d/1pz_MPwerlbM5--sAdBykbcImsRL-zNGezqLI69oX-ac/edit?usp=sharing')
     sheet = excel.worksheet_by_title(sheetName)
